dc7ccc7895116fecf4a660cf6f97165fad641a01
numark410 <j4k4l91@gmail.com>
2021-01-21 15:38:16 +0800

    change for v2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-Header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello I'm Numark</h1>
       </header>
     </div>
   );
